1aae2b911cd031722153a424fb01a86415faf94b
Mitch McAffee <themcaffee@gmail.com>
2021-11-16 23:53:36 -0700

    CORS

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-
-
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "https://santa.mitchmcaffee.com"}})
 
 
 @app.route("/")
