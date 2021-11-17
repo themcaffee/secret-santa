3d70b0d303e44580358cbfaf5623e7e8eb53fb6b
Mitch McAffee <themcaffee@gmail.com>
2021-11-17 02:15:35 -0700

    do the thing

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,14 +2,16 @@
   <div class="home">
     <b-card>
       <h3>Create New Secret Santa List</h3>
+      <b-form @submit.prevent="onSubmit">
         <b-form-group label="Group Name" label-for="name">
           <b-form-input id="name" v-model="formName" required></b-form-input>
         </b-form-group>
         <b-form-group label="Password (for sending out emails)" label-for="password">
-          <b-form-input id="password" type="password" v-model="formPassword" required></b-form-input>
+          <b-form-input id="password" type="password" v-model="formName" required></b-form-input>
         </b-form-group>
         <br>
-        <b-button block @click="onSubmit()" variant="primary">Submit</b-button>
+        <b-button block type="submit" variant="primary">Submit</b-button>
+      </b-form>
     </b-card>
   </div>
 </template>
@@ -24,16 +26,19 @@ export default {
       formPassword: ''
     }
   },
-  onSubmit () {
-    this.$http.post('https://santa-api.mitchmcaffee.com/list', {
-      name: this.formName,
-      password: this.formPassword
-    }).then(response => {
-      console.log(response.body)
-      this.$router.push({ name: 'SantaListView', params: { id: response.body.list.uuid } })
-    }, response => {
-      console.log(response)
-    })
+  methods: {
+    onSubmit (event) {
+      event.preventDefault()
+      this.$http.post('https://santa-api.mitchmcaffee.com/list', {
+        name: this.formName,
+        password: this.formPassword
+      }).then(response => {
+        console.log(response.body)
+        this.$router.push({ name: 'SantaListView', params: { id: response.body.list.uuid } })
+      }, response => {
+        console.log(response)
+      })
+    }
   }
 }
 </script>
--- web/src/views/SantaListView.vue
+++ web/src/views/SantaListView.vue
@@ -2,25 +2,23 @@
   <div class="santa-list-view">
     <b-card id="signup-form">
       <h4>Sign Up For Secret Santa</h4>
-      <b-form @submit="onSubmit">
-        <b-form-group label="Name" label-for="name">
-          <b-form-input id="name" v-model="formName" required></b-form-input>
-        </b-form-group>
-        <b-form-group label="Email" label-for="email">
-          <b-form-input id="email" v-model="formEmail" required></b-form-input>
-        </b-form-group>
-        <b-form-group label="Exclude from matching (Significant others)" label-for="exclude">
-          <b-form-select id="exclude" v-model="formExclude" :options="participantNames"></b-form-select>
-        </b-form-group>
-        <b-form-group label="Gift ideas & Interests" label-for="ideas">
-          <b-form-textarea
-            id="ideas"
-            placeholder="Write something good! You will not be able to change this later."
-            rows="8"></b-form-textarea>
-        </b-form-group>
-        <br>
-        <b-button block type="submit" variant="primary">Submit</b-button>
-      </b-form>
+      <b-form-group label="Name" label-for="name">
+        <b-form-input id="name" v-model="formName" required></b-form-input>
+      </b-form-group>
+      <b-form-group label="Email" label-for="email">
+        <b-form-input id="email" v-model="formEmail" required></b-form-input>
+      </b-form-group>
+      <b-form-group label="Exclude from matching (Significant others)" label-for="exclude">
+        <b-form-select id="exclude" v-model="formExclude" :options="participantNames"></b-form-select>
+      </b-form-group>
+      <b-form-group label="Gift ideas & Interests" label-for="ideas">
+        <b-form-textarea
+          id="ideas"
+          placeholder="Write something good! You will not be able to change this later."
+          rows="8"></b-form-textarea>
+      </b-form-group>
+      <br>
+      <b-button block @click="onSubmit()" variant="primary">Submit</b-button>
     </b-card>
     <b-card>
       <h3>{{ listName }}</h3>
@@ -71,7 +69,7 @@ export default {
     }
   },
   methods: {
-    onSubmit (event) {
+    onSubmit () {
       this.$http.post('https://santa-api.mitchmcaffee.com/list/' + this.listUuid + '/participant', {
         name: this.formName,
         email: this.formEmail,
@@ -83,7 +81,6 @@ export default {
       }, response => {
         console.log(response)
       })
-      event.preventDefault()
     },
     getList () {
       this.listUuid = this.$route.params.id
