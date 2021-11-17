e1b9ea41e7ce73543d483f4752a3509d80b366a3
Mitch McAffee <themcaffee@gmail.com>
2021-11-16 21:12:41 -0700

    Add dynamodb table

--- app.py
+++ app.py
@@ -8,3 +8,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/list", methods=['POST'])
+def create_list():
+  data = request.get_json()
+  print(data)
+  return jsonify(data)
+
+@app.route("/list", methods=['GET'])
+def get_list():
+  return jsonify({"list": [1, 2, 3, 4]})
