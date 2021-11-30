e2f85eaa31c4993f4d8845845a2ba4a24cbd5745
Mitch McAffee <themcaffee@gmail.com>
2021-11-29 23:50:56 -0700

    format email

--- app.py
+++ app.py
@@ -128,24 +128,19 @@ def create_participant(id):
 
 def send_email(recipient, gift_participant):
   # The subject line for the email.
-  subject = "Amazon SES Test (SDK for Python)"
+  subject = "Your Secret Santa has been selected!"
   # The email body for recipients with non-HTML email clients.
-  body_text = ("Amazon SES Test (Python)\r\n"
-              "This email was sent with Amazon SES using the "
-              "AWS SDK for Python (Boto)."
-              )
+  body_text = ("Secret Santa\r\n"
+              "You have been selected to be Secret Santa for {}!\r\n".format(gift_participant))
   # The HTML body of the email.
   body_html = """<html>
   <head></head>
   <body>
-    <h1>Amazon SES Test (SDK for Python)</h1>
-    <p>This email was sent with
-      <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
-      <a href='https://aws.amazon.com/sdk-for-python/'>
-        AWS SDK for Python (Boto)</a>.</p>
+    <h1>Secret Santa</h1>
+    <p>You have been selected to be Secret Santa for {}!</p>
   </body>
   </html>
-              """            
+              """.format(gift_participant)            
   # The character encoding for the email.
   charset = "UTF-8"
 
